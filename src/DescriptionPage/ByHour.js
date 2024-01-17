195b1e389f31a20aa06457ead5140c82747157c3
StepanTokt <stepantokt@mail.ru>
2024-01-17 22:38:30 +0300

    add carousel

--- src/DescriptionPage/ByHour.js
+++ src/DescriptionPage/ByHour.js
@@ -8,6 +8,10 @@ const ByHour = () => {
     const {getWeek, process, setProcess} = useWeatherService()
     const {city} = useParams()
     const {date} = useParams()
+
+    const [slideIndex, setSlideIndex] = useState(1)
+    const [slides, setSlides] = useState(8)
+
     useEffect(() => {
         updateCurrent()
     }, [city])
@@ -21,7 +25,7 @@ const ByHour = () => {
     const onCurrentLoaded = (current) => {
         const index = current.findIndex(item => item.date.num === date)
         if(index+6>40) index = 40
-        current = current.slice(index,index+6)
+        current = current.slice(index,index+8)
         setCurrent(current)
     }
 
@@ -32,21 +36,52 @@ const ByHour = () => {
                 <h1>По вашему запросу ничего не найдено</h1>
             </div>
         )
-        return weather.map(item => (
-            <div className="byHour_grid-item">
+        return weather.map((item,index) => (
+            <div 
+                key={item.id}
+                className={ slideIndex-1 === index || slideIndex - 1 === index - 1 || slideIndex - 1 === index - 2 || slideIndex - 1 === index - 3 || slideIndex - 1 === index - 4 || slideIndex - 1 === index - 5 ? 'show byHour_grid-item' : 'hide'}>
                 <div className="time">{(item.date.time).split(':').slice(0,2).join(":")}</div>
                 <img src={`https://openweathermap.org/img/wn/${item.icon}.png`} alt="" className="time_logo" />
                 <p className="time_temp">{item.temp}&deg;</p>
             </div>
+            
         ))
     }
 
     const elements = useMemo(() => {
         return setContentCurrent(process, () => renderWeather(current), null)
-    }, [process])
+    }, [process, slideIndex])
+
+
+    //CAROUSEL
+
+
+    
+    const plusSlides = (n) => {
+        console.log(slideIndex+n)
+        if(slideIndex+n === 0) setSlideIndex(1)
+        else if(slideIndex+n === 4) setSlideIndex(3)
+        else setSlideIndex(slideIndex+n)
+    }
 
     return(
         <section className="byHour">
+            <div className="carousel">
+                <div 
+                    className={slideIndex === 1 ? 'left gray' : 'left'}
+                    onClick={()=>plusSlides(-1)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+                        </svg>
+                    </div>
+                <div
+                    className={slideIndex === 3 ? 'right gray' : 'left'}
+                    onClick={()=>plusSlides(1)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+                        </svg>
+                    </div>
+            </div>
             <div className="byHour_grid">
                 
                 {elements}
